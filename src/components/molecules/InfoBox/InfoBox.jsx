d1176b36916f2a8873392ea93358487d4555365a
Kunuk Nykjær <kunukn@gmail.com>
2018-09-06 22:56:50 +0200

    use HOC

--- src/components/molecules/InfoBox/InfoBox.jsx
+++ src/components/molecules/InfoBox/InfoBox.jsx
@@ -1,29 +1,24 @@
 import './info-box.scss';
 import React from 'react';
 import { Button } from 'components/atoms';
-import { AppContext } from 'src/contexts';
+import { withAppState } from 'src/contexts';
 import { loadData, actionTypes } from 'src/actions';
 const { NAME, COUNT } = actionTypes;
 
-export default function InfoBox({ title, children }) {
-  return (
-    <AppContext.Consumer>
-      {appState => {
-        let onNameChange = e => appState.dispatch({ type: NAME, payload: e.target.value });
-        let onCountChange = e => appState.dispatch({ type: COUNT, payload: e.target.value });
-        let onLoadClick = () => loadData(appState.dispatch, 'https://jsonplaceholder.typicode.com/todos/1');
+function InfoBox({ title, children, appState }) {
+  let onNameChange = e => appState.dispatch({ type: NAME, payload: e.target.value });
+  let onCountChange = e => appState.dispatch({ type: COUNT, payload: e.target.value });
+  let onLoadClick = () => loadData(appState.dispatch, 'https://jsonplaceholder.typicode.com/todos/1');
 
-        return (
-          <div className="info-box">
-            <p className="info-box__title">{title}</p>
-            <p>{appState.version}</p>
-            <div>{children}</div>
-            <input type="text" placeholder="type name" value={appState.name} onChange={onNameChange} />
-            <input type="number" placeholder="type count" value={appState.count} onChange={onCountChange} />
-            <Button onClick={onLoadClick}>load data</Button>
-          </div>
-        );
-      }}
-    </AppContext.Consumer>
+  return (
+    <div className="info-box">
+      <p className="info-box__title">{title}</p>
+      <div>{children}</div>
+      <input type="text" placeholder="type name" value={appState.name} onChange={onNameChange} />
+      <input type="number" placeholder="type count" value={appState.count} onChange={onCountChange} />
+      <Button onClick={onLoadClick}>load data</Button>
+    </div>
   );
 }
+
+export default withAppState(InfoBox);
--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
 
 export const AppContext = React.createContext();
+
+export function withAppState(Component) {
+  return function AppStateComponent(props) {
+    return <AppContext.Consumer>{appState => <Component {...props} appState={appState} />}</AppContext.Consumer>;
+  };
+}
