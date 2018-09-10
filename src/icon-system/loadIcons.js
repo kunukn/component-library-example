0a1061941bb5b6e4a7c5e70fc0ee2143e4a7691b
Kunuk Nykjær <kunyk@dfds.com>
2018-09-10 16:24:34 +0200

    upd

--- src/icon-system/loadIcons.js
+++ src/icon-system/loadIcons.js
@@ -3,16 +3,25 @@ export default function loadIcons({ path, callback, delay } = {}) {
   let d = document;
   let b = d.body;
   ajax.onload = () => {
-    let div = d.createElement('div');
-    div.style.display = 'none';
-    div.className = 'svg-in-the-dom';
-    div.innerHTML = ajax.responseText;
-    b.insertBefore(div, b.childNodes[0]);
-    callback && callback(ajax.responseText);
+    if (ajax.status === 200) {
+      let div = d.createElement('div');
+      div.style.display = 'none';
+      div.className = 'svg-in-the-dom';
+      div.innerHTML = ajax.responseText;
+      b.insertBefore(div, b.childNodes[0]);
+    }
+
+    callback && callback(ajax);
   };
 
-  setTimeout(() => {
+  function invoke() {
     ajax.open('GET', path || '/icons.svg', true);
     ajax.send();
-  }, delay || 0);
+  }
+
+  if (delay) {
+    setTimeout(invoke, delay);
+  } else {
+    invoke();
+  }
 }
