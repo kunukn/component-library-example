aaff6dac916d4df31ad4cf331cb0fdab94200708
Kunuk Nykjær <kunukn@gmail.com>
2018-10-24 23:10:36 +0200

    explicitly set components bundle

--- src/production-entry.js
+++ src/production-entry.js
@@ -1,6 +1,11 @@
 import 'components/styles';
 
-import { DataLoader, Icon, Button } from 'components';
 import loadIcons from 'src/icon-system/loadIcons';
+
+import DataLoader from 'components/atoms/DataLoader/DataLoader';
+import Icon from 'components/atoms/Icon/Icon';
+import Button from 'components/molecules/Button/Button';
+
+console.log(!!loadIcons, !!Button, !!Icon, !!DataLoader);
 
 export { loadIcons, DataLoader, Button, Icon };
